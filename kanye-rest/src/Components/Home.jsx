c54eea7d42105fab5004fcc1bbf65b8adfb0348b
ChapmanChappelle <chapman.chappelle@gmail.com>
2021-10-20 17:58:30 -0400

    kanye api

--- kanye-rest/src/Components/Home.jsx
+++ kanye-rest/src/Components/Home.jsx
@@ -1,11 +1,27 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./Home.css"
-
-
 export const Home = () => {
+   const [quote, setQuote] = useState({})
+
+   useEffect(() => {
+      getQuote()
+   }, [])
+
+   const getQuote = async () => {
+      const response = await fetch("https://api.kanye.rest/")
+      const data = await response.json()
+
+      setQuote(data)
+      return "success"
+   }
+
    return (
       <div id="container">
-         
+         <div id="header">
+            <h1>Kanye Rest API</h1>
+         </div>
+         <button onClick="getQuote()" id="getQuoteBtn">Ye Quote</button>
+         <p id="quoteTxt">{quote.quote}</p>
       </div>
    )
 }
